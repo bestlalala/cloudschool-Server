5319c863f74b84382314682399109676a40b1ede
yeonsu <bestdustn@sookmyung.ac.kr>
2024-11-27 22:24:35 +0900

    [CONFIG] Add CORS configuration for API endpoints - Configure CORS to allow requests from frontend

--- src/main/java/com/bestlalala/cloudschool/UserController.java
+++ src/main/java/com/bestlalala/cloudschool/UserController.java
@@ -3,6 +3,7 @@ package com.bestlalala.cloudschool;
 import com.bestlalala.cloudschool.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class UserController {
     private final UserService userService;
 
     /* User 테이블 전체 조회 API */
+    @CrossOrigin(origins = "http://localhost:3000")  // React 앱 주소로 CORS 허용
     @GetMapping("/api/user")
     public ResponseEntity<List<UserDto>> getUserInfo() {
         List<UserDto> allUsers = userService.findAllUsers();
