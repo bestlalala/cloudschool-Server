200c6d72f72fa552c23bc5bf23a62bd91058d41a
yeonsu <bestdustn@sookmyung.ac.kr>
2024-11-29 01:55:19 +0900

    [CONFIG] Update CORS

--- src/main/java/com/bestlalala/cloudschool/UserController.java
+++ src/main/java/com/bestlalala/cloudschool/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +16,8 @@ public class UserController {
     private final UserService userService;
 
     /* User 테이블 전체 조회 API */
-    @CrossOrigin(origins = "http://localhost:3000")  // React 앱 주소로 CORS 허용
-    @GetMapping("/api/user")
+    @CrossOrigin(origins = "http://cloudschool-bucket.s3-website.ap-northeast-2.amazonaws.com")  // React 앱 주소로 CORS 허용
+    @GetMapping("/")
     public ResponseEntity<List<UserDto>> getUserInfo() {
         List<UserDto> allUsers = userService.findAllUsers();
         return ResponseEntity.ok(allUsers);
